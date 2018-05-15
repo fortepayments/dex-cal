4666565a906563d28f62b1812b77b14bf9fc6e23
Mithun Patel <mithunster@gmail.com>
2018-05-15 13:26:43 -0500

    Switch build to use double quotes for templateUrl. ESLint switches single quotes to double

--- inline-resources.js
+++ inline-resources.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
-
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
 
 /**
  * Simple Promiseify function that takes a Node API and return a version that supports promises.
@@ -11,16 +10,21 @@ const glob = require('glob');
  * faster. It also simplifies the code.
  */
 function promiseify(fn) {
-  return function () {
+  return function() {
     const args = [].slice.call(arguments, 0);
     return new Promise((resolve, reject) => {
-      fn.apply(this, args.concat([function (err, value) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(value);
-        }
-      }]));
+      fn.apply(
+        this,
+        args.concat([
+          function(err, value) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(value);
+            }
+          }
+        ])
+      );
     });
   };
 }
@@ -33,23 +37,26 @@ const writeFile = promiseify(fs.writeFile);
  * @param projectPath {string} Path to the project.
  */
 function inlineResources(projectPath) {
-
   // Match only TypeScript files in projectPath.
-  const files = glob.sync('**/*.ts', {cwd: projectPath});
+  const files = glob.sync("**/*.ts", { cwd: projectPath });
 
   // For each file, inline the templates and styles under it and write the new file.
-  return Promise.all(files.map(filePath => {
-    const fullFilePath = path.join(projectPath, filePath);
-    return readFile(fullFilePath, 'utf-8')
-      .then(content => inlineResourcesFromString(content, url => {
-        // Resolve the template url.
-        return path.join(path.dirname(fullFilePath), url);
-      }))
-      .then(content => writeFile(fullFilePath, content))
-      .catch(err => {
-        console.error('An error occured: ', err);
-      });
-  }));
+  return Promise.all(
+    files.map(filePath => {
+      const fullFilePath = path.join(projectPath, filePath);
+      return readFile(fullFilePath, "utf-8")
+        .then(content =>
+          inlineResourcesFromString(content, url => {
+            // Resolve the template url.
+            return path.join(path.dirname(fullFilePath), url);
+          })
+        )
+        .then(content => writeFile(fullFilePath, content))
+        .catch(err => {
+          console.error("An error occured: ", err);
+        });
+    })
+  );
 }
 
 /**
@@ -60,11 +67,10 @@ function inlineResources(projectPath) {
  */
 function inlineResourcesFromString(content, urlResolver) {
   // Curry through the inlining functions.
-  return [
-    inlineTemplate,
-    inlineStyle,
-    removeModuleId
-  ].reduce((content, fn) => fn(content, urlResolver), content);
+  return [inlineTemplate, inlineStyle, removeModuleId].reduce(
+    (content, fn) => fn(content, urlResolver),
+    content
+  );
 }
 
 /**
@@ -75,16 +81,18 @@ function inlineResourcesFromString(content, urlResolver) {
  * @return {string} The content with all templates inlined.
  */
 function inlineTemplate(content, urlResolver) {
-  return content.replace(/templateUrl:\s*'([^']+?\.html)'/g, function (m, templateUrl) {
+  return content.replace(/templateUrl:\s*"([^']+?\.html)"/g, function(
+    m,
+    templateUrl
+  ) {
     const templateFile = urlResolver(templateUrl);
-    const templateContent = fs.readFileSync(templateFile, 'utf-8');
+    const templateContent = fs.readFileSync(templateFile, "utf-8");
     const shortenedTemplate = templateContent
-      .replace(/([\n\r]\s*)+/gm, ' ')
+      .replace(/([\n\r]\s*)+/gm, " ")
       .replace(/"/g, '\\"');
     return `template: "${shortenedTemplate}"`;
   });
 }
-
 
 /**
  * Inline the styles for a source file. Simply search for instances of `styleUrls: [...]` and
@@ -94,22 +102,27 @@ function inlineTemplate(content, urlResolver) {
  * @return {string} The content with all styles inlined.
  */
 function inlineStyle(content, urlResolver) {
-  return content.replace(/styleUrls:\s*(\[[\s\S]*?\])/gm, function (m, styleUrls) {
+  return content.replace(/styleUrls:\s*(\[[\s\S]*?\])/gm, function(
+    m,
+    styleUrls
+  ) {
     const urls = eval(styleUrls);
-    return 'styles: ['
-      + urls.map(styleUrl => {
-        const styleFile = urlResolver(styleUrl);
-        const styleContent = fs.readFileSync(styleFile, 'utf-8');
-        const shortenedStyle = styleContent
-          .replace(/([\n\r]\s*)+/gm, ' ')
-          .replace(/"/g, '\\"');
-        return `"${shortenedStyle}"`;
-      })
-        .join(',\n')
-      + ']';
+    return (
+      "styles: [" +
+      urls
+        .map(styleUrl => {
+          const styleFile = urlResolver(styleUrl);
+          const styleContent = fs.readFileSync(styleFile, "utf-8");
+          const shortenedStyle = styleContent
+            .replace(/([\n\r]\s*)+/gm, " ")
+            .replace(/"/g, '\\"');
+          return `"${shortenedStyle}"`;
+        })
+        .join(",\n") +
+      "]"
+    );
   });
 }
-
 
 /**
  * Remove every mention of `moduleId: module.id`.
@@ -117,7 +130,7 @@ function inlineStyle(content, urlResolver) {
  * @returns {string} The content with all moduleId: mentions removed.
  */
 function removeModuleId(content) {
-  return content.replace(/\s*moduleId:\s*module\.id\s*,?\s*/gm, '');
+  return content.replace(/\s*moduleId:\s*module\.id\s*,?\s*/gm, "");
 }
 
 module.exports = inlineResources;
@@ -125,6 +138,6 @@ module.exports.inlineResourcesFromString = inlineResourcesFromString;
 
 // Run inlineResources if module is being called directly from the CLI with arguments.
 if (require.main === module && process.argv.length > 2) {
-  console.log('Inlining resources from project:', process.argv[2]);
+  console.log("Inlining resources from project:", process.argv[2]);
   return inlineResources(process.argv[2]);
 }
